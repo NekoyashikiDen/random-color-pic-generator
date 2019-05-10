4ddb5b89b6cddbaa777fde0ed807b0d529caaf90
NekoyashikiDen <dexanderlee@uir.edu.cn>
2019-05-10 21:29:46 +0800

    annotation

--- randomcolor.m
+++ randomcolor.m
@@ -1,14 +1,15 @@
-width=100
+width=100 % width & length of the pic
 length=100
-seed=1234;
+
+seed=1234; % random seed of the generator
 rng(seed,'twister');
 
 pic=zeros(width,length,3);
 temp=zeros(1,3);
-r=32;
+r=32; % amount of generated values in each channel
 g=32;
 b=32;
-for m=randperm(256,r)-1
+for m=randperm(256,r)-1 % generate random values
     for n=randperm(256,g)-1
         for l=randperm(256,b)-1
             temp(1,:)=[m n l];
@@ -19,8 +20,8 @@ for m=randperm(256,r)-1
                   end
                end
             end
-            pic=uint8(pic);
-            code=lower([dec2hex(temp(1),2),dec2hex(temp(2),2),dec2hex(temp(3),2)]);
+            pic=uint8(pic); % transfer the matrix into pic file
+            code=lower([dec2hex(temp(1),2),dec2hex(temp(2),2),dec2hex(temp(3),2)]); % hex file name
             name=['0x',code,'.jpg'];
             imwrite(pic,name);
         end
